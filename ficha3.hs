77f38f9cabff16da6b3bc7629f85e70162e8f8d5
João Duarte <up201707984@up.pt>
2022-10-13 12:16:54 +0100

    updated

--- ficha3.hs
+++ ficha3.hs
@@ -6,3 +6,28 @@ mapfilter f p l = map f (filter p l)
 
 --3.2
 dec2int :: [Int] -> Int
+dec2int [] = 0
+dec2int (x:xs) = foldl (\x y -> x*10 + y) 0 (x:xs)
+
+-- 3.3 
+
+zipWith10 :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith10 _ [] _ = []
+zipWith10 _ _ [] = []
+zipWith10 f (x:xs) (y:ys) = f x y : zipWith10 f xs ys
+
+-- 3.4 
+
+isort :: Ord a => [a] -> [a]
+isort  
+
+-- 3.5 
+
+{-
+foldl1 = maximum xs = foldl1 max xs 
+foldr1 = maximum xs = foldr1 max xs 
+foldl1 = minimum xs = foldl1 min xs 
+foldr1 = minimum xs = foldr1 min xs 
+-}
+
+-- 
