55fcd161d9837f1bd812ce273ebff737f2596c47
João Duarte <52889593+joaotudojunto@users.noreply.github.com>
2023-01-14 15:59:30 +0000

    going

--- Prolog/ficha3/1.pl
+++ Prolog/ficha3/1.pl
@@ -35,7 +35,7 @@ print_n(S, N) :- N > 0, write(S), N1 is N-1, print_n(S, N1).
 
 % b 
 print_string(""). 
-print_string([Code | String]) :- char_code(Char, Code), write(Char), print_string(String).
+print_string([Code | String]) :- put_code(Code), print_string(String).
 
 print_text(T, S, P) :-  write(S), print_n(' ', P), print_string(T), print_n(' ', P), write(S).
 
@@ -48,6 +48,11 @@ print_banner(T,S,P) :- P >=0,
                         print_text(T,S,P), nl, 
                         print_blank_spaces(T,S,P), nl, 
                         print_banner_edge(T, S, P), nl. 
+
+%e 
+read_until_between(Min, Max, Value) :- read_number(Value), X > Min, X < Max, !. 
+read_until_between(Min, Max, Value) :- read_until_between(Min, Max, Value). 
+
 
 %f 
 read_string("") :- peek_code(10), !, get_code(_).
@@ -68,8 +73,10 @@ banner :- print_string("Text = "),
 %------------  ex 5 
 
 % a) 
+children(Person,Children) :- findall(Child, parent(Parent,Child), Children). 
 
-children(Person, Children) :- findall(Child, parent(Person, Child), Children). 
+%b) 
+children_of(ListOfPeople, ListOfPairs) :- findall(Pair, (member(P, ListOfPeople), children(P, C), Pair = P-C), ListOfPairs).
 
 %c) 
 connected(P1, P2) :- parent(P1, P2); parent(P2, P1).
@@ -86,3 +93,63 @@ spouse_children(Person, Spouse/Children) :- couple(Person-Spouse), children(Pers
 
 %g) 
 immediate_family(Person, Parents-SC) :- findall(Parent, parent(Parent, Person), Parents), spouse_children(Person, SC).
+
+% ex 6 
+
+leciona(algoritmos, adalberto).
+leciona(bases_de_dados, bernardete).
+leciona(compiladores, capitolino).
+leciona(estatistica, diogenes).
+leciona(redes, ermelinda).
+
+
+frequenta(algoritmos, alberto).
+frequenta(algoritmos, bruna).
+frequenta(algoritmos, cristina).
+frequenta(algoritmos, diogo).
+frequenta(algoritmos, eduarda).
+
+frequenta(bases_de_dados, antonio).
+frequenta(bases_de_dados, bruno).
+frequenta(bases_de_dados, cristina).
+frequenta(bases_de_dados, duarte).
+frequenta(bases_de_dados, eduardo).
+
+frequenta(compiladores, alberto).
+frequenta(compiladores, bernardo).
+frequenta(compiladores, clara).
+frequenta(compiladores, diana).
+frequenta(compiladores, eurico).
+
+frequenta(estatistica, antonio).
+frequenta(estatistica, bruna).
+frequenta(estatistica, claudio).
+frequenta(estatistica, duarte).
+frequenta(estatistica, eva).
+
+frequenta(redes, alvaro).
+frequenta(redes, beatriz).
+frequenta(redes, claudio).
+frequenta(redes, diana).
+frequenta(redes, eduardo).
+
+aluno(X, Y) :- frequenta(UC, X), leciona(UC, Y).
+colega(X, Y, UC) :- frequenta(UC, X), frequenta(UC, Y), (X \= Y).
+
+%a 
+teachers(T) :- findall(Teacher, leciona(UC,Teacher), T).
+
+%b
+teacherDup(T) :- setof(Teacher, UC^leciona(UC,Teacher), T).  
+
+%c 
+students_of(T, S) :- setof(S, (leciona(UC,T), frequenta(UC,S)), S).
+
+%d 
+teachers_of(S, T) :- findall(T, (leciona(UC,T), frequenta(UC,S)), T). 
+
+%e
+common_courses(S1,S2,C) :- findall(UC, (frequenta(UC,S1), frequenta(UC,S2)), C). 
+
+%F
+more_than_one_course(L) :- setof(S, (frequenta(UC1, S), frequenta(UC2,S), UC1 \= UC2), L).
